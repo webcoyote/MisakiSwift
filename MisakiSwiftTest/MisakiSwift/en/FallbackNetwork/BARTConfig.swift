a5810a0df87aaffa46ff66b130662c712c4ff959
mlalma <lassi.maksimainen@gmail.com>
2025-09-21 16:03:45 +0200

    Added BART configuration loading depending on if using US or GB lexicon

--- MisakiSwiftTest/MisakiSwift/en/FallbackNetwork/BARTConfig.swift
+++ MisakiSwiftTest/MisakiSwift/en/FallbackNetwork/BARTConfig.swift
@@ -21,45 +21,56 @@ struct BARTConfig: Codable {
   let encoderLayers: Int
   let eosTokenId: Int
   let forcedEosTokenId: Int
-  let graphemeChars: [String]
+  let graphemeChars: String
   let id2label: [String: String]
   let initStd: Double
   let isEncoderDecoder: Bool
-  
+  let label2id: [String: Int]
+  let maxPositionEmbeddings: Int
+  let modelType: String
+  let numHiddenLayers: Int
+  let padTokenId: Int
+  let phonemeChars: String
+  let scaleEmbedding: Bool
+  let torchDType: String
+  let transformersVersion: String
+  let useCache: Bool
   let vocabSize: Int
-    let maxPositionEmbeddings: Int
-    let padTokenId: Int
-    let phonemeChars: [String]
     
-    enum CodingKeys: String, CodingKey {
-      case activationDropout = "activation_dropout"
-      case activationFunction = "activation_function"
-      case architectures = "architectures"
-      case vocabSize = "vocab_size"
-      case attentionDropout = "attention_dropout"
-      case bosTokenId = "bos_token_id"
-      case classifierDropout = "classifier_daropout"
-      case dModel = "d_model"
-      case decoderAttentionHeads = "decoder_attention_heads"
-      case decoderFFNDim = "decoder_ffn_dim"
-      case decoderLayerDrop = "decoder_layerdrop"
-      case decoderLayers = "decoder_layers"
-      case decoderStartTokenId = "decoder_start_token_id"
-      case dropout = "dropout"
-      case encoderAttentionHeads = "encoder_attention_heads"
-      case encoderFFNDim = "encoder_ffn_dim"
-      case encoderLayerDrop = "encoder_layerdrop"
-      case encoderLayers = "encoder_layers"
-      case eosTokenId = "eos_token_id"
-      case forcedEosTokenId = "forced_eos_token_id"
-      case graphemeChars = "grapheme_chars"
-      case id2label = "id2label"
-      case initStd = "init_std"
-      case isEncoderDecoder = "isEncoderDecoder"
-      
-
-        case maxPositionEmbeddings = "max_position_embeddings"
-        case padTokenId = "pad_token_id"
-        case phonemeChars = "phoneme_chars"
-    }
+  enum CodingKeys: String, CodingKey {
+    case activationDropout = "activation_dropout"
+    case activationFunction = "activation_function"
+    case architectures = "architectures"
+    case attentionDropout = "attention_dropout"
+    case bosTokenId = "bos_token_id"
+    case classifierDropout = "classifier_dropout"
+    case dModel = "d_model"
+    case decoderAttentionHeads = "decoder_attention_heads"
+    case decoderFFNDim = "decoder_ffn_dim"
+    case decoderLayerDrop = "decoder_layerdrop"
+    case decoderLayers = "decoder_layers"
+    case decoderStartTokenId = "decoder_start_token_id"
+    case dropout = "dropout"
+    case encoderAttentionHeads = "encoder_attention_heads"
+    case encoderFFNDim = "encoder_ffn_dim"
+    case encoderLayerDrop = "encoder_layerdrop"
+    case encoderLayers = "encoder_layers"
+    case eosTokenId = "eos_token_id"
+    case forcedEosTokenId = "forced_eos_token_id"
+    case graphemeChars = "grapheme_chars"
+    case id2label = "id2label"
+    case initStd = "init_std"
+    case isEncoderDecoder = "is_encoder_decoder"
+    case label2id = "label2id"
+    case maxPositionEmbeddings = "max_position_embeddings"
+    case modelType = "model_type"
+    case numHiddenLayers = "num_hidden_layers"
+    case padTokenId = "pad_token_id"
+    case phonemeChars = "phoneme_chars"
+    case scaleEmbedding = "scale_embedding"
+    case torchDType = "torch_dtype"
+    case transformersVersion = "transformers_version"
+    case useCache = "use_cache"
+    case vocabSize = "vocab_size"
+  }
 }
--- MisakiSwiftTest/MisakiSwift/en/FallbackNetwork/EnglishFallbackNetwork.swift
+++ MisakiSwiftTest/MisakiSwift/en/FallbackNetwork/EnglishFallbackNetwork.swift
@@ -1,11 +1,31 @@
+import Foundation
+
 class EnglishFallbackNetwork {
+  private let configuration: BARTConfig?
   private let british: Bool
     
   init(british: Bool) {
+    do {
+      configuration = try EnglishFallbackNetwork.loadConfig(british: british)!
+    } catch(let ex) {
+      configuration = nil
+      print("Exception \(ex.localizedDescription)")
+    }
     self.british = british
   }
   
   func callAsFunction(_ word: MToken) -> (phoneme: String, rating: Int) {
     return ("[Dunno]", 4)
+  }
+  
+  private static func loadConfig(british: Bool) throws -> BARTConfig? {
+    let filename = "\(british ? "gb" : "us")_bart_config"
+    guard let url = Bundle(for: EnglishFallbackNetwork.self).url(forResource: filename, withExtension: "json"),
+          let data = try? Data(contentsOf: url) else {
+        return nil
+    }
+    
+    let config = try JSONDecoder().decode(BARTConfig.self, from: data)
+    return config
   }
 }
