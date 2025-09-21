d9ebd1372111b0a4588daa9c30f8223d89e2eb7c
mlalma <lassi.maksimainen@gmail.com>
2025-09-21 16:35:31 +0200

    Added the BARt model weights and loading them directly to MLXArray

--- MisakiSwiftTest/MisakiSwift/en/FallbackNetwork/EnglishFallbackNetwork.swift
+++ MisakiSwiftTest/MisakiSwift/en/FallbackNetwork/EnglishFallbackNetwork.swift
@@ -1,16 +1,15 @@
 import Foundation
+import MLX
 
 class EnglishFallbackNetwork {
   private let configuration: BARTConfig?
+  private let modelWeights: [String: MLXArray]
+  
   private let british: Bool
     
   init(british: Bool) {
-    do {
-      configuration = try EnglishFallbackNetwork.loadConfig(british: british)!
-    } catch(let ex) {
-      configuration = nil
-      print("Exception \(ex.localizedDescription)")
-    }
+    configuration = EnglishFallbackNetwork.loadConfig(british: british)!
+    modelWeights = EnglishFallbackNetwork.loadWeights(british: british)!
     self.british = british
   }
   
@@ -18,14 +17,23 @@ class EnglishFallbackNetwork {
     return ("[Dunno]", 4)
   }
   
-  private static func loadConfig(british: Bool) throws -> BARTConfig? {
-    let filename = "\(british ? "gb" : "us")_bart_config"
-    guard let url = Bundle(for: EnglishFallbackNetwork.self).url(forResource: filename, withExtension: "json"),
-          let data = try? Data(contentsOf: url) else {
+  private static func loadConfig(british: Bool) -> BARTConfig? {
+    let fileName = "\(british ? "gb" : "us")_bart_config"
+    guard let url = Bundle(for: EnglishFallbackNetwork.self).url(forResource: fileName, withExtension: "json"),
+          let data = try? Data(contentsOf: url),
+          let config = try? JSONDecoder().decode(BARTConfig.self, from: data) else {
         return nil
     }
-    
-    let config = try JSONDecoder().decode(BARTConfig.self, from: data)
     return config
+  }
+  
+  private static func loadWeights(british: Bool) -> [String: MLXArray]? {
+    let fileName = "\(british ? "gb" : "us")_bart"
+    let filePath = Bundle(for: EnglishFallbackNetwork.self).path(forResource: fileName, ofType: "safetensors")!
+    guard let weights = try? MLX.loadArrays(url: URL(fileURLWithPath: filePath)) else {
+      return nil
+    }
+    
+    return weights
   }
 }
